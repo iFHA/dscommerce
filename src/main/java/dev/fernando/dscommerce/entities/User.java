79597ec229d0910a3800a0a8fc5bc80b8c81893e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 13:20:13 -0300

    added user entity

--- src/main/java/dev/fernando/dscommerce/entities/User.java
+++ src/main/java/dev/fernando/dscommerce/entities/User.java
@@ -1,17 +1,31 @@
 package dev.fernando.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
