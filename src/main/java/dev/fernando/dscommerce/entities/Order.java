6b1172a67b6072cfd61fcf154273c129c24976b8
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-06 13:20:21 -0300

    added order entity

--- src/main/java/dev/fernando/dscommerce/entities/Order.java
+++ src/main/java/dev/fernando/dscommerce/entities/Order.java
@@ -1,23 +1,39 @@
 package dev.fernando.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.List;
 
 import dev.fernando.dscommerce.enums.OrderStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_order")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
     private User client;
+
+    @ManyToMany(targetEntity = OrderItem.class)
+    private List<Product> products;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Payment payment;
 }
