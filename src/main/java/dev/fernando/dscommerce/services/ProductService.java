074e9ca45e84f86877f40b8e268baebeda873b1b
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-10 13:05:13 -0300

    transactional

--- src/main/java/dev/fernando/dscommerce/services/ProductService.java
+++ src/main/java/dev/fernando/dscommerce/services/ProductService.java
@@ -3,6 +3,7 @@ package dev.fernando.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.fernando.dscommerce.dto.ProductDTO;
 import dev.fernando.dscommerce.entities.Product;
@@ -16,6 +17,7 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Optional<Product> product = this.productRepository.findById(id);
         Product p = product.get();
