c7140bf35de0fe59fbe9a560f8892800bcc92767
iFHA <fernando.everywhere@gmail.com>
2024-10-16 18:43:43 -0300

    product service findall test

--- src/test/java/dev/fernando/dscommerce/services/ProductServiceTest.java
+++ src/test/java/dev/fernando/dscommerce/services/ProductServiceTest.java
@@ -1,16 +1,23 @@
 package dev.fernando.dscommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import dev.fernando.dscommerce.dto.ProductMinDTO;
 import dev.fernando.dscommerce.entities.Product;
 import dev.fernando.dscommerce.repositories.ProductRepository;
 import dev.fernando.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -28,15 +35,18 @@ public class ProductServiceTest {
     private Long existingId;
     private Long nonExistingId;
     private Product entity;
+    private PageImpl<Product> page;
 
     @BeforeEach
     void setUp() throws Exception {
         this.existingId = 1L;
         this.nonExistingId = 1000L;
         this.entity = ProductFactory.createProduct();
+        this.page = new PageImpl<>(List.of(this.entity));
 
         Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(entity));
         Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+        Mockito.when(repository.searchByName(ArgumentMatchers.anyString(), (Pageable) ArgumentMatchers.any())).thenReturn(page);
     }
 
     @Test
@@ -47,5 +57,12 @@ public class ProductServiceTest {
     @Test
     void findByIdShouldThrowResourceNotFoundExceptionWhenProductIdDoesNotExist() {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> service.findById(nonExistingId));
+    }
+    @Test
+    void findAllShoudlReturnPagedProductMinDto() {
+        var result = service.findAll("produto", PageRequest.of(1, 1));
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1, result.getSize());
+        Assertions.assertInstanceOf(ProductMinDTO.class, result.getContent().get(0));
     }
 }
